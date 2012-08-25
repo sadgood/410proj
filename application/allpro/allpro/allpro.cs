3817743464a469ddb58a632871214fd6080be2e0
jerryhethatday <jerryhe@jerryhe.com>
2012-08-25 22:21:40 +0800

    allpro---功能性已经基本上实现
    a productive weekend,已经可以在工序间进行尺寸链的校核啦。哈哈，明天开始就要把这些东西都配置到黄一波的电脑里面，。哈哈

--- application/allpro/allpro/allpro.cs
+++ application/allpro/allpro/allpro.cs
@@ -72,7 +72,19 @@ public class allpro
     public static readonly int     SnapPointTypesOnByDefault_BoundedGridPoint = (1 <<16);
     public ArrayList dimary = new ArrayList();//这个动态数组存放所有需要进行校核的尺寸，很重要。
     public NXOpen.Annotations.Dimension[] dimarydim;//与dimary对应的数组
-    
+    public NXOpen.TaggedObject[] theoripmi;//要校核的PMI
+    public ArrayList addcir = new ArrayList();//存储增环
+    public ArrayList deccir = new ArrayList();//存储减环
+    public ArrayList thedown = new ArrayList();//记录整个工序内除过要校核尺寸外的尺寸
+    List<int[]> lst_Combination = new List<int[]>();//list用add方法时也要用new分配内存
+    double finalx = 0;//对应一个尺寸属性列表中的x分值，下同。
+    double finaly = 0;
+    double finalz = 0;
+    NXOpen.Annotations.Dimension[] thefinalori = null;//不预先设定校核范围时，最后存储尺寸的变量
+    DatumAxis xformone = null;//要校核的尺寸生成的轴
+    NXOpen.Annotations.Dimension[] left = null;//存放除过要校核的之外的所有尺寸
+    public ArrayList theday = new ArrayList();//theday是一个里面存放排列组合后得到的尺寸的数组
+    public List<NXOpen.Annotations.Dimension[]> finaloneinpro = new List<NXOpen.Annotations.Dimension[]>();//校核的时候存放和要校核的尺寸成环的其他所有尺寸的list
     //------------------------------------------------------------------------------
     //Constructor for NX Styler class
     //------------------------------------------------------------------------------
@@ -236,6 +248,12 @@ public class allpro
         try
         {
             selectPart0.GetProperties().SetLogical("Enable", false);
+            tree_control0.InsertColumn(1, "尺寸链", 130);//一定有注意不同的回调函数的问题
+            tree_control0.InsertColumn(2, "名义尺", 100);
+            tree_control0.InsertColumn(3, "上公差", 100);
+            tree_control0.InsertColumn(4, "下公差", 100);
+            tree_control0.InsertColumn(5, "增/减环", 100);
+            tree_control0.InsertColumn(6,"所在部件",100);
         }
         catch (Exception ex)
         {
@@ -265,17 +283,35 @@ public class allpro
     //------------------------------------------------------------------------------
     public ArrayList getpartlist(string path)////////得到一个文件夹里面所有prt的全路径
     {
-        string[] prtinfo;
+        //string[] prtinfo = {"",""};//第一个
         ArrayList prtinfoary = new ArrayList();
         FileInfo[] prtfile;
         DirectoryInfo profolder = new DirectoryInfo(path);
         prtfile = profolder.GetFiles("*.prt");
         foreach (FileInfo eachprt in prtfile)
         {
-            //eachprt.FullName
+            ////eachprt.FullName
             prtinfoary.Add(eachprt.FullName);
+            //eachprt.Name
+            //prtinfo[0] = eachprt.FullName;//第一个是全路径，第二个是文件名
+            //prtinfo[1] = eachprt.Name;
+            //prtinfoary.Add(prtinfo);
+           
         }
         return prtinfoary;
+    }
+    public static T Tag2NXObject<T>(Tag tag)
+    {
+        try
+        {
+            object to = NXOpen.Utilities.NXObjectManager.Get(tag);
+            return (T)to;
+        }
+        catch (System.Exception ex)
+        {
+            UI.GetUI().NXMessageBox.Show("Message", NXMessageBox.DialogType.Error, ex.Message);
+            return default(T);
+        }
     }
     public int update_cb( NXOpen.BlockStyler.UIBlock block)
     {
@@ -308,8 +344,26 @@ public class allpro
               ArrayList prtnameary = new ArrayList();//存放所有在工艺文件夹下的prt的全路径
               string path = nativeFolderBrowser0.GetProperties().GetString("Path");
               prtnameary = getpartlist(path);//得到所有在工艺文件夹下的prt的全路径
+              Part thework = theSession.Parts.Display;//当前工作部件的路径
+              string workpath = thework.FullPath;
+              theoripmi = selection0.GetProperties().GetTaggedObjectVector("SelectedObjects");  //需要校核的尺寸
+              NXOpen.Annotations.Dimension theoridim = Tag2NXObject<NXOpen.Annotations.Dimension>(theoripmi[0].Tag);
+              xformone = creataxis(theoridim);//需要校核的尺寸所生成的轴
+              hideit((NXObject)xformone);//隐藏需要校核的尺寸所生成的轴
+              finalx = theoridim.GetRealAttribute("X");
+              finaly = theoridim.GetRealAttribute("Y");
+              finalz = theoridim.GetRealAttribute("Z");
+              foreach (NXOpen.Annotations.Dimension workdim in thework.Dimensions)
+              {
+                  if (workdim != theoridim)
+                  {
+                  dimary.Add(workdim);
+                  }
+              }
                 foreach(string loadpath in prtnameary)
                 {
+                    if (loadpath != workpath)
+                    {
                     PartLoadStatus loadcondition;
                     Part thetempart;//这个存放每个暂时打开的prt文件，然后收集其中的PMI尺寸
                     thetempart = theSession.Parts.Open(loadpath,out loadcondition);
@@ -318,9 +372,114 @@ public class allpro
                     foreach (NXOpen.Annotations.Dimension eachdim in thetempart.Dimensions.ToArray())
                     {
                         dimary.Add(eachdim);
-                    
+                        FileInfo prtname = new FileInfo(loadpath);
+                        string realprtname = prtname.Name;
+                        eachdim.SetAttribute("所属部件",realprtname);
+                    }
+
                     }
                 }
+                List<int[]> nene;
+                left = (NXOpen.Annotations.Dimension[])dimary.ToArray(typeof(NXOpen.Annotations.Dimension));//把动态数组转化成数组
+                int[] arr = new int[left.Length];//下面这个for循环定义了一个索引数组，里面存放的是left这个数组的索引。一一对应
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = i;
+                }
+                for (int t = 2; t <= left.Length; t++)//该循环从2开始，因为一个尺寸连最起码有三个，它也有可能达到left数组的长度。
+                {
+
+                    nene = Algorithms.PermutationAndCombination<int>.GetCombination(arr, t);//nene是每一次循环得到的结果，如果直接用nene参与下一步计算，以前的循环结果就作废了
+                    foreach (int[] ne in nene)
+                    {
+                        lst_Combination.Add(ne);
+                    }
+
+                }
+
+                foreach (int[] a in lst_Combination)//遍历list里面存的索引数组
+                {
+                    theday.Clear();
+                    for (int j = 0; j < a.Length; j++)//下面这个for循环从索引得到对应的dimension数组。
+                    {
+
+                        theday.Add(left[a[j]]);//这一步有问题。。。。。妈的。------这里一个小错误就让我调试了一早上
+                        // thefinalori[j] = left[j];//得到索引所表示的数组
+
+                    }
+                    thefinalori = (NXOpen.Annotations.Dimension[])theday.ToArray(typeof(NXOpen.Annotations.Dimension));
+                    if (checknow(thefinalori))//如果成环的话。。。。almost there
+                    {
+                        finaloneinpro.Add(thefinalori);//这个list存放的是所有和需校核尺寸成环的尺寸链，记住其里面的每个元素是一个尺寸数组
+
+                    };
+
+                }
+                count ct = new count();
+                ArrayList zengzu = new ArrayList();
+                ArrayList jianzu = new ArrayList();
+                NXOpen.Annotations.Dimension[] zengshuzu = null;
+                NXOpen.Annotations.Dimension[] jianshuzu = null;
+                NXOpen.BlockStyler.Node finalnode = null;
+                foreach (NXOpen.Annotations.Dimension[] ori in finaloneinpro)//这部分终于搞定了。哈哈，很高兴啊。
+                {
+                    jianzu.Clear();
+                    zengzu.Clear();
+                    finalnode = tree_control0.CreateNode("成环尺寸链");
+                    tree_control0.InsertNode(finalnode, null, null, Tree.NodeInsertOption.Last);
+                    foreach (NXOpen.Annotations.Dimension sda in ori)
+                    {
+                        NXOpen.BlockStyler.Node finalchild = tree_control0.CreateNode("NodeData");
+                        DataContainer nodeData = finalchild.GetNodeData();
+                        int p = 0;
+                        double[] final = { 0, 0, 0 };
+                        nodeData.AddTaggedObject("Data", sda);
+                        nodeData.Dispose();
+                        tree_control0.InsertNode(finalchild, finalnode, null, Tree.NodeInsertOption.Last);
+                        p = cirdect(sda);
+                        if (p == -1)
+                        {
+
+                            finalchild.SetColumnDisplayText(5, "减环");
+                            jianzu.Add(sda);
+
+                        }
+                        if (p == 1)
+                        {
+                            finalchild.SetColumnDisplayText(5, "增环");
+                            zengzu.Add(sda);
+                        }
+                        if (p == 0)
+                        {
+                            finalchild.SetColumnDisplayText(5, "对封闭环无贡献");
+                        }
+                        final = ct.getspec(sda);
+                        finalchild.SetColumnDisplayText(2, final[0].ToString());
+                        finalchild.SetColumnDisplayText(3, final[1].ToString());
+                        finalchild.SetColumnDisplayText(4, final[2].ToString());
+                        Part sdapart = (Part)sda.OwningPart;
+                        FileInfo sdafile = new FileInfo(sdapart.FullPath);
+                        string prtname = sdafile.Name;
+                        finalchild.SetColumnDisplayText(6,prtname);
+                    }
+                    zengshuzu = (NXOpen.Annotations.Dimension[])zengzu.ToArray(typeof(NXOpen.Annotations.Dimension));
+                    jianshuzu = (NXOpen.Annotations.Dimension[])jianzu.ToArray(typeof(NXOpen.Annotations.Dimension));
+                    if (ct.countcircle(zengshuzu, jianshuzu, theoridim))
+                    {
+                        //tree_control0.InsertColumn(1, "尺寸链", 100);//一定有注意不同的回调函数的问题
+
+                        finalnode.SetColumnDisplayText(1, "符合尺寸链规则");
+                        finalnode.ForegroundColor = 198;//红色表示未通过尺寸链校核
+                    }
+                    else
+                    {
+                        finalnode.SetColumnDisplayText(1, "不符合尺寸链规则");
+                        finalnode.ForegroundColor = 198;
+                    }
+
+                }
+
+
             }
         }
         catch (Exception ex)
@@ -347,7 +506,265 @@ public class allpro
         }
         return errorCode;
     }
-    
+    public NXObject shank(Point point1, Point point2)
+    {
+        Part workPart = theSession.Parts.Work;
+        NXObject shanker;
+        NXOpen.Features.Feature nullFeatures_Feature = null;
+        NXOpen.Features.DatumAxisBuilder abuilder;
+        abuilder = workPart.Features.CreateDatumAxisBuilder(nullFeatures_Feature);
+        abuilder.ArcLength.Expression.RightHandSide = "0";
+        abuilder.Type = NXOpen.Features.DatumAxisBuilder.Types.TwoPoints;
+        abuilder.IsAssociative = true;
+        Xform nullXform = null;
+        Point pt1 = workPart.Points.CreatePoint(point1, nullXform, NXOpen.SmartObject.UpdateOption.WithinModeling);
+        Point pt2 = workPart.Points.CreatePoint(point2, nullXform, NXOpen.SmartObject.UpdateOption.WithinModeling);
+        abuilder.Point1 = pt1;
+        abuilder.Point2 = pt2;
+        shanker = abuilder.Commit();
+        abuilder.Destroy();
+
+        return shanker;
+
+    }
+    public DatumAxis creataxis(NXOpen.Annotations.Dimension dimen)//这个函数直接从一个dimension得到一个它的轴
+    {
+        Part workPart = theSession.Parts.Work;
+        double startx = 0;
+        double starty = 0;
+        double startz = 0;
+        double endx = 0;
+        double endy = 0;
+        double endz = 0;
+        startx = dimen.GetRealAttribute("START-X");
+        starty = dimen.GetRealAttribute("START-Y");
+        startz = dimen.GetRealAttribute("START-Z");
+        endx = dimen.GetRealAttribute("END-X");
+        endy = dimen.GetRealAttribute("END-Y");
+        endz = dimen.GetRealAttribute("END-Z");
+        Point3d stpt;
+        stpt.X = startx;
+        stpt.Y = starty;
+        stpt.Z = startz;
+        Point3d endpt;
+        endpt.X = endx;
+        endpt.Y = endy;
+        endpt.Z = endz;
+        Point realstart = workPart.Points.CreatePoint(stpt);//得到这个PMI矢量的起点
+        Point realend = workPart.Points.CreatePoint(endpt);//得到这个PMI矢量的终点
+        NXOpen.Features.DatumAxisFeature fiansis = (NXOpen.Features.DatumAxisFeature)shank(realstart, realend);
+        // fiansis.DatumAxis;
+        DatumAxis fian = fiansis.DatumAxis;
+        return fian;
+    }
+    public double anglemethod(DatumAxis a, DatumAxis b)
+    {
+
+        Part workPart = theSession.Parts.Work;
+        NXObject nullNXObject = null;
+        MeasureAngleBuilder bbuilder;
+        bbuilder = workPart.MeasureManager.CreateMeasureAngleBuilder(nullNXObject);
+        bbuilder.Object1.Value = a;
+        bbuilder.Object2.Value = b;
+        Unit nullUnit = null;
+        MeasureAngle measureAngle1;
+        measureAngle1 = workPart.MeasureManager.NewAngle(nullUnit, a, NXOpen.MeasureManager.EndpointType.None, b, NXOpen.MeasureManager.EndpointType.None, true, false);
+        double deg = measureAngle1.Value;
+        return deg;
+    }
+    public static double ConvertDegreesToRadians(double degrees)//角度到弧度的转换方法
+    {
+        double radians = (Math.PI / 180) * degrees;
+        // return (radians);
+        return radians;//return 的这两种写法都是可以的
+    }
+    public void hideit(NXObject objtohide)//////这是一个隐藏NXObject的方法
+    {
+        DisplayableObject a = (DisplayableObject)objtohide;
+        DisplayableObject[] objects1 = new DisplayableObject[1];
+        objects1[0] = a;
+        theSession.DisplayManager.BlankObjects(objects1);
+
+    }
+    static List<int[]> GetPermutation(int h, int t)
+    {
+        int[] result = new int[t];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = i;
+        }
+        List<int[]> resultlist = new List<int[]>();
+        resultlist.Add(result);
+        while (result != null)
+        {
+            result = GetNextResult(result, h);
+            if (result != null)
+            {
+                resultlist.Add(result);
+            }
+        }
+        return resultlist;
+    }
+    static int[] GetNextResult(int[] result, int h)
+    {
+        int[] nextresult = new int[result.Length];
+        result.CopyTo(nextresult, 0);
+        bool bAdd = true;
+        for (int i = result.Length - 1; i >= 0; i--)
+        {
+
+            if (bAdd)
+            {
+
+                nextresult[i] = result[i] + 1;
+
+                bAdd = false;
+            }
+            else
+            {
+                nextresult[i] = result[i];
+            }
+
+
+            List<int> checkrepeat = new List<int>(nextresult);
+            checkrepeat.RemoveAt(i);
+            while (checkrepeat.Contains(nextresult[i]))
+            {
+                nextresult[i]++;
+            }
+
+            if (nextresult[i] >= h)
+            {
+                if (i == 0) return null;
+                bAdd = true;
+                nextresult[i] = 0;
+                checkrepeat.RemoveAt(i);
+                while (checkrepeat.Contains(nextresult[i]))
+                {
+                    nextresult[i]++;
+                }
+            }
+        }
+
+
+        return nextresult;
+    }
+    public bool checknow(NXOpen.Annotations.Dimension[] finalallpmi)//这个函数判断这个dimension数组是否和要校核的尺寸成环
+    {
+        for (int i = 0; i < finalallpmi.Length; i++)
+        {
+
+            NXOpen.Annotations.Dimension ok = Tag2NXObject<NXOpen.Annotations.Dimension>(finalallpmi[i].Tag);
+            finalx = finalx + ok.GetRealAttribute("X");
+            finaly = finaly + ok.GetRealAttribute("Y");
+            finalz = finalz + ok.GetRealAttribute("Z");
+            // dimarylist.Add(ok);
+
+        }
+        bool wetx = (Math.Abs(finalx)) <= 0.000000000001;
+        bool wety = (Math.Abs(finaly)) <= 0.000000000001;
+        bool wetz = (Math.Abs(finalz)) <= 0.000000000001;
+        bool puanduan = (wetx && wety && wetz);
+        theoripmi = selection0.GetProperties().GetTaggedObjectVector("SelectedObjects");  //需要校核的尺寸
+        NXOpen.Annotations.Dimension theoridim = Tag2NXObject<NXOpen.Annotations.Dimension>(theoripmi[0].Tag);
+        finalx = theoridim.GetRealAttribute("X");
+        finaly = theoridim.GetRealAttribute("Y");
+        finalz = theoridim.GetRealAttribute("Z");
+        return puanduan;
+    }
+    public void setappendzeng(NXOpen.Annotations.Dimension zengdim)//true为增环，false为减环,zengdim为要加文本的
+    {
+
+        Part workPart = theSession.Parts.Work;
+        Part displayPart = theSession.Parts.Display;
+        NXOpen.Annotations.DimensionPreferences dimensionPreferences1;
+        dimensionPreferences1 = zengdim.GetDimensionPreferences();
+        NXOpen.Annotations.AppendedText appendedText1;
+        appendedText1 = workPart.Annotations.NewAppendedText();
+        string[] lines4 = new string[1];
+
+        lines4[0] = "增";
+        appendedText1.SetAfterText(lines4);
+        zengdim.SetAppendedText(appendedText1);
+        appendedText1.Dispose();
+
+
+    }
+    public void setappendjian(NXOpen.Annotations.Dimension zengdim)//true为增环，false为减环,zengdim为要加文本的
+    {
+
+        Part workPart = theSession.Parts.Work;
+        Part displayPart = theSession.Parts.Display;
+        NXOpen.Annotations.DimensionPreferences dimensionPreferences1;
+        dimensionPreferences1 = zengdim.GetDimensionPreferences();
+        NXOpen.Annotations.AppendedText appendedText1;
+        appendedText1 = workPart.Annotations.NewAppendedText();
+        string[] lines4 = new string[1];
+
+        lines4[0] = "减";
+        appendedText1.SetAfterText(lines4);
+        zengdim.SetAppendedText(appendedText1);
+
+        appendedText1.Dispose();
+
+
+
+    }
+    public int cirdect(NXOpen.Annotations.Dimension cirdick)//这个方法判断一个尺寸对于要校核的是增环还是减环，或者垂直无贡献
+    {
+        // -1为减环，0为无贡献，1为增环
+        int aa = 0;
+        DatumAxis correct = creataxis(cirdick);
+        double anglllll = anglemethod(correct, xformone);
+        hideit((NXObject)correct);
+        if (anglllll > 90)
+        {
+
+            aa = -1;
+        }
+        if (anglllll < 90)
+        {
+
+            aa = 1;
+
+        }
+        if (anglllll == 90)
+        {
+
+            aa = 0;
+        }
+        return aa;
+
+
+    }
+    public void afandfor(NXOpen.Annotations.Dimension[] a, out NXOpen.Annotations.Dimension[] b, out NXOpen.Annotations.Dimension[] c)//这个函数对一个数组中的增减环进行筛选，注意这个数组中的尺寸应该已经和需要校核的尺寸成环
+    {
+        ///////////////////其中a是输入进来的需要进行增减环赛选的尺寸数组，b是减环，c是增环
+        ArrayList vv = new ArrayList();//存放减环
+        ArrayList bb = new ArrayList();//存放增环
+        int qq = 0;
+        foreach (NXOpen.Annotations.Dimension f in a)
+        {
+            qq = cirdect(f);
+            if (qq == -1)
+            {
+                vv.Add(f);//加入减环
+            }
+            if (qq == 1)
+            {
+                bb.Add(f);//加入增环
+            }
+            if (qq == 0)
+            {
+
+
+            }
+
+        }
+        b = (NXOpen.Annotations.Dimension[])vv.ToArray(typeof(NXOpen.Annotations.Dimension));
+        c = (NXOpen.Annotations.Dimension[])bb.ToArray(typeof(NXOpen.Annotations.Dimension));
+
+    }
     //------------------------------------------------------------------------------
     //Callback Name: cancel_cb
     //------------------------------------------------------------------------------
---
+++ application/allpro/allpro/count.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using NXOpen;
+using NXOpen.BlockStyler;
+using NXOpenUI;
+using NXOpen.UF;
+using System.Collections;
+using System.Collections.Generic;
+public class count
+{
+    
+    public bool countcircle(NXOpen.Annotations.Dimension[] zeng,NXOpen.Annotations.Dimension[] jian,NXOpen.Annotations.Dimension fengbi)//这个函数是进行尺寸链校核计算的主方法
+    {
+       double zengmax = 0;//所有增环的最大尺寸之和
+       double zengmin = 0;//所有增环的最小尺寸之和
+       double jianmax = 0;//所有减环的最大尺寸之和
+       double jianmin = 0;//所有减环的最小尺寸之和
+       double clsmax = 0;//封闭环最大尺寸
+       double clsmin =0;//封闭环最小尺寸
+       double aclsmax = 0;//所有 增环最大尺寸减去所有减环最小极限尺寸
+       double aclsmin = 0;//所有增环最小尺寸减去所有减环最大尺寸
+        foreach (NXOpen.Annotations.Dimension zenghuan in zeng)
+        {
+            zengmax = getspec(zenghuan)[0] + getspec(zenghuan)[1];
+            zengmin = getspec(zenghuan)[0] + getspec(zenghuan)[2];
+        }
+        foreach(NXOpen.Annotations.Dimension jianhuan in jian)
+        {
+            jianmax = getspec(jianhuan)[0] + getspec(jianhuan)[1];
+            jianmin = getspec(jianhuan)[0] + getspec(jianhuan)[2];
+        }
+     
+        clsmax = getspec(fengbi)[0] + getspec(fengbi)[1];
+        clsmin = getspec(fengbi)[0] + getspec(fengbi)[2];
+        aclsmax = zengmax - jianmin;
+        aclsmin = zengmin - jianmin;
+        if (aclsmax >= clsmax && aclsmin <= clsmin)
+        {
+            return true;
+        }
+        else {
+            return false;
+
+        }
+
+    }
+    public double[] getspec(NXOpen.Annotations.Dimension dim)//返回一个尺寸的名义值和上下公差，第一个值是名义值，第二个是上公差，第三个是下公差
+    {
+         string[] a;
+         string[] b;
+         double maindim = 0;
+         double low = 0;
+         double up = 0;
+         double[] final = {0,0,0};
+         dim.GetDimensionText(out a, out b);
+         maindim = double.Parse(a[0]);
+         up = dim.UpperMetricToleranceValue;
+         low = dim.LowerMetricToleranceValue;
+         final[0] = maindim;
+         final[1] = up;
+         final[2] = low;
+         return final;
+    }
+    
+}
